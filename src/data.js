546f96220d06edc8b65c9c8f08524087ab2dc4d7
Rushikeshpowar121 <technify.1nor@gmail.com>
2024-09-22 23:34:12 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const data = [
+    {
+      question: "Which device is required for the Internet connection?",
+      a: "Modem",
+      b: "Router",
+      c: "LAN Cable",
+      option4: "Pen Drive",
+      ans: "Modem" ,
+    },
+    {
+      question: "Which continent has the highest number of countries?",
+      a: "Asia",
+      b: "Europe",
+      c: "North America",
+      option4: "Africa",
+      ans: "Africa",
+    },
+    {
+      question: "Junk e-mail is also called?",
+      a: "Spam",
+      b: "Fake",
+      c: "Archived",
+      option4: "Bin",
+      ans: "Spam",
+    },
+  ];
